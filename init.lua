d736f9dbb109d7a1fb8e3177bfa2055cff4c3ff0
Arjun Gore <arjun.c.gore@gmail.com>
2026-01-15 19:29:25 -0500

    test

---
+++ init.lua
@@ -0,0 +1,35 @@
+-- ==== VARS ====
+local waywall = require("waywall")
+local helpers = require("helpers")
+--
+-- local cfg = {
+--     key = "MB5",
+--     chat_func = "Home"
+--     norm_func = "F13"
+-- }
+
+local M       = {}
+-- ==== PLUG ====
+M.setup       = function(config, cfg)
+    local remaps_norm = {} -- No Chat
+    local remaps_chat = {} -- Yes Chat
+    for key, val in pairs(config.input.remaps) do
+        remaps_norm[key] = val
+        remaps_chat[key] = val
+    end
+
+    if remaps_norm[cfg.key] ~= cfg.chat_func then
+        remaps_chat[cfg.key] = cfg.chat_func
+    end
+    remaps_norm[cfg.key] = cfg.norm_func
+
+    waywall.listen("state", function()
+        if not helpers.ingame_only() then
+            waywall.set_remaps(remaps_norm)
+        else
+            waywall.set_remaps(remaps_chat)
+        end
+    end)
+end
+
+return M
